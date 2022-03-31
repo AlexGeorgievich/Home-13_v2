210aff03f322e5210e381a2012c571749e205b25
AlexGeorgievich <alexhome1@yandex.ru>
2022-03-31 12:38:06 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+## Проект по тестированию  страниц сайта компании "Т1 Консалтинг", – российский разработчик и интегратор, входит в Группу Т1.
+> <a target="_blank" href="https://www.t1-consulting.ru/">Ссылка на главную страницу сайта</a>
+
+
+### :drop_of_blood: Содержание:
+
+- [Технологии и инструменты](#earth_africa-технологии-и-инструменты)
+- [Реализованные проверки](#earth_africa-Реализованные-проверки)
+- [Сборка в Jenkins](#earth_africa-Jenkins-job)
+- [Запуск из терминала](#earth_africa-Запуск-тестов-из-терминала)
+- [Allure отчет](#earth_africa-Allure-отчет)
+- [Отчет в Telegram](#earth_africa-Уведомление-в-Telegram-при-помощи-бота)
+- [Видео примеры прохождения тестов](#earth_africa-Примеры-видео-о-прохождении-тестов)
+
+
+### Проект реализован с использованием
+
+### Список проверок, реализованных в автотестах
+- [x] Наличие  заголовка и меню на главной странице
+- [x] Наличие заданных пунктов подменю в разделе "О компании" и проверка девиза компании
+- [x] Проверка главного меню сайта с переменными названиями разделов меню, их наполнение страниц разделов
+- [x] Наличие раздела "О компании" и достоверности раздела меню 
+- [x] Проверка  меню и наполнение разделов меню "О компании"
+- [x] Проверка наличия и доступности к скачиванию инструкции по эксплуатации системы "T1 Watchman"
+- [x] Проверка на наличие ошибок в console log
+
+
+### For run remote tests need fill remote.properties or to pass value:
+
+* browser (default chrome)
+* browserVersion (default 89.0)
+* browserSize (default 1920x1080)
+* browserMobileView (mobile device name, for example iPhone X)
+* remoteDriverUrl (url address from selenoid or grid)
+* videoStorage (url address where you should get video)
+* threads (number of threads)
+
+
+Run tests with filled remote.properties:
+```bash
+gradle clean test
+```
+
+Run tests with not filled remote.properties:
+```bash
+gradle clean -DremoteDriverUrl=https://%s:%s@selenoid.autotests.cloud/wd/hub/ -DvideoStorage=https://selenoid.autotests.cloud/video/ -Dthreads=1 test
+```
+
+Serve report:
+```bash
+allure serve build/allure-results
+```
+ 
+:heart: <a target="_blank" href="https://qa.guru">qa.guru</a><br/>
+:blue_heart: <a target="_blank" href="https://t.me/qa_automation">t.me/qa_automation</a>
---
+++ src/test/java/cloud/tests/Tconsulting.java
@@ -0,0 +1,149 @@
+package cloud.tests;
+
+import cloud.helps.Attach;
+import com.codeborne.pdftest.PDF;
+import com.codeborne.selenide.Selenide;
+import io.qameta.allure.Description;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.File;
+
+import static com.codeborne.selenide.CollectionCondition.texts;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Selectors.byLinkText;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Tconsulting extends TestBase{
+
+    @Test
+    @DisplayName("Проверка наличия заголовка на главной странице сайта ")
+    void controlTitleTest() {
+        step("Открытие сайта по адресу - " + baseUrl, () ->  {open(baseUrl);} );
+        step("Заголовок страницы должен содержать название компании '«Т1 Консалтинг»'", () -> {
+            String expectedTitle = "«Т1 Консалтинг»";
+            String actualTitle = title();
+            assertThat(actualTitle).isEqualTo(expectedTitle);
+        });
+        step("Проверка наличия меню не главной странице ", () -> {
+            $$("div.menu-top")
+                    .shouldBe(texts("О компании Услуги Карта решений Продукты Контакты"));
+        });
+    }
+
+    @Test
+    @Description("Проверка доступности сайта и переход в раздел -О компании-, <motto of company> ")
+    @DisplayName("Проверка раздела о компании  ")
+    void whatMottoCompanyIsTest() {
+        step("Открытие сайта по адресу - " + baseUrl, () -> {
+            open(baseUrl);
+        } );
+        step(" Позиционирование по меню сайта в раздел О компании", () -> {
+            $(byLinkText("О компании")).click();
+            $x("//h1[contains(text(),'О компании')]").shouldBe(visible);
+        });
+        step("Проверка декларации Миссии и <motto> компании ", ()-> {
+            $$("div.l-about")
+                    .shouldHave(texts("Быть доверенным партнером и проводником в цифровой мир для компаний, стремящихся стать лучшими в своем деле."));
+        });
+    }
+
+    // Проверка главного меню сайта с переменными названиями разделов меню, их наполнение страниц разделов
+    @ValueSource(strings = {"О компании", "Услуги", "Карта решений","Продукты","Контакты"})
+    @ParameterizedTest(name = "Проверяем наличие пункта - \"{0}\" в меню навигации по сайту")
+    void mainMenuTest(String subject) {
+        step("Открытие сайта по адресу -  " + baseUrl, () -> {
+            openPageOfSite(baseUrl);
+        } );
+        step( "Позиционируем по меню, пункт  - " + subject , () ->
+                $(byLinkText(subject)).click());
+        step("проверка наличия раздела " , () ->
+                $x("//h1[contains(text(),subject)]").shouldBe(visible));
+    }
+
+    @Test
+    @DisplayName("Позиционирование по разделу <О компании> и определение наличия и достоверности меню раздела")
+    void menuAboutCompanyTest() {
+        step("Открытие  сайта по адресу - " + baseUrl, () -> {
+            openPageOfSite(baseUrl);
+        } );
+        step( "Позиционируем по меню, пункт  - ", () ->
+                $(byLinkText("О компании")).click());
+        step("Проверка наличия меню в разделе 'О компании' ", () -> {
+            $$("div.menu-inner")
+                    .shouldBe(texts("Пресс-центр Достижения Команда Партнеры Группа компаний Кодекс деловой этики"));
+        });
+    }
+
+    //    Проверка  меню, переход по меню и контроль наполнения разделов
+    @ValueSource(strings = {"Пресс-центр", "Достижения", "Команда","Партнеры","Группа компаний","Кодекс деловой этики"})
+    @ParameterizedTest(name = "Проверяем наличие пункта - \"{0}\" в меню навигации по сайту в разделе <О компании>")
+    void whatWeKnowAboutCompanyTest(String subject) {
+        step("Открытие  сайта по адресу - " + baseUrl, () -> {
+            open(baseUrl+ "about/");
+        } );
+        step( "Позиционируем по меню, пункт  - " + subject , () ->
+                $(byLinkText(subject)).click());
+        step("проверка наличия раздела и его наполнении" , () ->
+                $x("//h1[contains(text(),subject)]").shouldBe(visible));
+    }
+
+    @Test
+    @DisplayName("Проверка наличия и доступности к скачиванию инструкции по эксплуатации системы <T1 Watchman>")
+    void upLoadAndParsePdfTest() throws Exception {
+        step("Открытие раздела содержащего материалы к скачиванию", () -> {
+            Selenide.open("https://www.t1-consulting.ru/services/middl/tsc-business-monitoring/");
+        });
+        step("Скачивание инструкции по эксплуатации системы  <T1 Watchman>", () -> {
+            File pdfDownload = Selenide.$("div.txt_leftcol").$(byText("Инструкция по эксплуатации")).download();
+            PDF pdf = new PDF(pdfDownload);
+            assertThat(pdf.author).contains("Elena M. Zhemerova"); //Проверка файла PDF на автора-разработчика инструкции
+            assertThat(pdf.numberOfPages).isEqualTo(77);           //число страниц инструкции
+            assertThat(pdf.text).contains("T1 Watchman", "Инструкция по эксплуатации"); //содержание текста в инструкции
+        });
+    }
+
+    @Test
+    @Description("Проверка на ошибки")
+    @DisplayName("Проверка на наличие ошибок в console log ")
+    void consoleShouldNotHaveErrorsTest() {
+        step("Открытие сайта по адресу - " + baseUrl, () -> {
+            open(baseUrl);
+        } );
+        step("Console logs should not contain text 'SEVERE'", () -> {
+            String consoleLogs = Attach.getConsoleLogs();
+            String errorText = "SEVERE";
+            assertThat(consoleLogs).doesNotContain(errorText);
+        });
+    }
+
+    @Test
+    @DisplayName("Проверка поиска по сайту")
+    void successfulSearchTest() {
+//        step("Открытие сайта по адресу - " + baseUrl, () -> {
+//            open(baseUrl);
+//        } );
+        open(baseUrl);
+        $("[name=q]").setValue("Watchman").pressEnter();
+        $("div.t-achiement").shouldHave(text("Watchman"));
+    }
+
+    void openPageOfSite(String urlSite) {
+        step("Открытие сайта - 'https://www.t1-consulting.ru/'", () ->
+                open(urlSite));
+    }
+
+    void searchDataAndControl(String subject) {
+        $(byLinkText(subject)).click();
+        $x("//h1[contains(text(),subject)]").shouldBe(visible);
+    }
+}
+
+
