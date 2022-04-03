aafe1f95a1bfec39e0435c891877a7e6f7bee02c
AlexGeorgievich <alexhome1@yandex.ru>
2022-04-03 18:26:38 +0300

    Changed auth.properties

--- src/test/java/cloud/config/AuthConfig.java
+++ src/test/java/cloud/config/AuthConfig.java
@@ -4,7 +4,7 @@ import org.aeonbits.owner.Config;
 
 @Config.Sources({
         "file:/tmp/auth.properties",
-        "classpath:auth.properties"
+        "classpath:/config/auth.properties"
 })
 public interface AuthConfig extends Config {
 
